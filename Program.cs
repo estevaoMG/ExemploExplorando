9c89703b01032db6f6a5efb69ed9e9cf934f16a5
estevaoMG <estevao.gouveia@hotmail.com>
2024-10-19 19:49:20 -0300

    Implementando o método listar alunos

--- Program.cs
+++ Program.cs
@@ -3,5 +3,62 @@
 Pessoa p1 = new Pessoa();
 p1.Nome = "Estevão";
 p1.Sobrenome = "Gouveia";
-p1.Idade = 31;
-p1.Apresentar();
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "Eduardo";
+p2.Sobrenome = "Neves Queriroz";
+
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Estevão";
+// p1.Sobrenome = "Gouveia";
+// p1.Idade = 31;
+// p1.Apresentar();
