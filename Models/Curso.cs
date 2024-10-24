017658d1d7de7c4df9bc3c894f8e44749d0debe8
estevaoMG <estevao.gouveia@hotmail.com>
2024-10-24 18:59:26 -0300

    Interpolação de Strings

--- Models/Curso.cs
+++ Models/Curso.cs
@@ -28,10 +28,14 @@ namespace ExemploExplorando.Models
 
         public void ListarAlunos()
         {
+
             Console.WriteLine($"Alunos do curso de: {Nome}");
-            foreach (Pessoa aluno in Alunos)
+            
+            for (int count = 0; count < Alunos.Count; count++)
             {
-                Console.WriteLine(aluno.NomeCompleto);
+                // string texto = "Nº" + count + " - " + Alunos[count].NomeCompleto;
+                string texto = $"Nº{count} - {Alunos[count].NomeCompleto}";
+                Console.WriteLine(texto);
             }
         }
     }
