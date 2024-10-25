b630bc94940ac537b75f71cfe6520a33497f0e85
estevaoMG <estevao.gouveia@hotmail.com>
2024-10-25 16:19:17 -0300

    Concatenação de valores

--- Program.cs
+++ Program.cs
@@ -1,17 +1,69 @@
 ﻿using ExemploExplorando.Models;
 
-Pessoa p1 = new Pessoa(nome: "Estevão", sobrenome:"Gouveia");
+string numero1 = "10";
+string numero2 = 20;
 
-Pessoa p2 = new Pessoa(nome: "Eduardo", sobrenome: "Neves Queiroz");
+string resultado = numero1 + numero2;
+
+Console.WriteLine(resultado);
 
 
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.Nome = "Inglês";
-cursoDeIngles.Alunos = new List<Pessoa>();
 
-cursoDeIngles.AdicionarAluno(p1);
-cursoDeIngles.AdicionarAluno(p2);
-cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa(nome: "Estevão", sobrenome:"Gouveia");
+
+// Pessoa p2 = new Pessoa(nome: "Eduardo", sobrenome: "Neves Queiroz");
+
+
+// Curso cursoDeIngles = new Curso();
+// cursoDeIngles.Nome = "Inglês";
+// cursoDeIngles.Alunos = new List<Pessoa>();
+
+// cursoDeIngles.AdicionarAluno(p1);
+// cursoDeIngles.AdicionarAluno(p2);
+// cursoDeIngles.ListarAlunos();
 
 
 
