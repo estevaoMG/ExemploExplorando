3dbf5eb336eab03abb61eab685a32f6b676ae710
estevaoMG <estevao.gouveia@hotmail.com>
2025-09-20 15:55:16 -0300

    Atributos

--- Models/Venda.cs
+++ Models/Venda.cs
@@ -2,20 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ExemploExplorando.Models
 {
     public class Venda
     {
-        public Venda(int id, string produto, decimal preco)
-        {
-            Id = id;
-            Produto = produto;
-            Preco = preco;
-        }
-
         public int Id { get; set; }
+
+        [JsonProperty("Nome_Produto")]
         public string Produto { get; set; }
+
         public decimal Preco { get; set; }
+        public DateTime DataVenda { get; set; }        
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,13 +2,85 @@
 using System.Globalization;
 using Newtonsoft.Json;
 
-Venda v1 = new Venda(1, "Material de escritório", 25.00M);
+string conteudoArquivo = File.ReadAllText("Arquivos/venda.json");
 
-string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
+List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
 
-File.WriteAllText("Arquivos/venda.json", serializado);
+foreach (Venda venda in listaVenda)
+{
+    Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}," +
+                      $" Preço: {venda.Preco}, Data: {venda.DataVenda.ToString("dd/MM/yyyy HH:mm")}");
+}
 
-Console .WriteLine(serializado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DateTime dataAtual = DateTime.Now;
+
+// List<Venda> listaVendas = new List<Venda>();
+
+// Venda v1 = new Venda(1, "Material de escritório", 25.00M, dataAtual);
+// Venda v2 = new Venda(2, "Licença de Software", 110.00M, dataAtual);
+
+// listaVendas.Add(v1);
+// listaVendas.Add(v2);
+
+
+// string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+
+// File.WriteAllText("Arquivos/venda.json", serializado);
+
+// Console .WriteLine(serializado);
 
 
 
